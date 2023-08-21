4591d5068cd2d65cd2bc738639389ec98645de9a
Harsh Mishra <imharshmishra.28@gmail.com>
2023-08-21 23:06:04 +0530

    Matrix Row Sum

---
+++ RowWiseSum.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[3][3];
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    int maxSum = 0;
+    int maxRowNumber = -1;
+    for (int i = 0; i < 3; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < 3; j++)
+        {
+            sum += arr[i][j];
+        }
+        if (sum > maxSum)
+        {
+            maxSum = sum;
+            maxRowNumber = i;
+        }
+        cout << "Sum of row no. " << i << " is " << sum << endl;
+    }
+
+    cout << "Row no. " << maxRowNumber << " has the highest sum" << endl;
+}
